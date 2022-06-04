7f9803777a0cf191b581e6bbbd508df77b06aa0f
candy <cvtenoriog@gmail.com>
2022-06-04 11:32:51 -0300

    teste simples presage e qt

--- src/portuguese/train_big_portuguese.sh
+++
@@ -1,4 +0,0 @@
-rm portuguese.db
-./text2ngram -n 1 -l -f sqlite -o portuguese.db portuguese_Dasher_with_phrases.txt
-./text2ngram -n 2 -l -f sqlite -o portuguese.db portuguese_Dasher_with_phrases.txt
-./text2ngram -n 3 -l -f sqlite -o portuguese.db portuguese_Dasher_with_phrases.txt
